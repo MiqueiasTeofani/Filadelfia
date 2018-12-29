7311082c8a7dee4202fe358434b98ffd9c259646
Miqueias Teofani <miqueiasteofaniB@outlook.com>
2018-12-28 21:05:59 -0300

    Adicionar arquivos de projeto.

---
+++ Filadelfia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Filadelfia.Startup))]
+namespace Filadelfia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
